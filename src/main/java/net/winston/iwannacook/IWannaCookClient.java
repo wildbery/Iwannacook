73a8e4c4d6c71945017eb97cc73b4ae965d05612
wildbery <103512355+wildbery@users.noreply.github.com>
2024-06-26 16:23:09 +0900

    Initial commit

---
+++ src/main/java/net/winston/iwannacook/IWannaCookClient.java
@@ -0,0 +1,10 @@
+package net.winston.iwannacook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IWannaCookClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/winston/iwannacook/IWannaCookDataGenerator.java
@@ -0,0 +1,12 @@
+package net.winston.iwannacook;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IWannaCookDataGenerator implements DataGeneratorEntrypoint {
+	// Hello
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
